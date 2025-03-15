fce93ef9c09c6334e18adde3c42c193b48958bfc
Essam-sara <saraessam2580@gmail.com>
2025-03-15 03:04:51 +0200

    edit login componant

--- frontend/iti-platform/src/app/login/login.component.ts
+++ frontend/iti-platform/src/app/login/login.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from '../services/auth.service';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   email: string = '';
