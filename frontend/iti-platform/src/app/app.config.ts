d2ffd6053501492670e8a4af44918bd243e6a7bd
Essam-sara <saraessam2580@gmail.com>
2025-03-16 07:28:39 +0200

    ⚙️ Configure app providers and settings

--- frontend/iti-platform/src/app/app.config.ts
+++ frontend/iti-platform/src/app/app.config.ts
@@ -2,10 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';  
 import { provideRouter } from '@angular/router';  // Importing the router configuration function
 
 import { routes } from './app.routes';  // Importing routes from the app.routes.ts file
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),  // Enabling change detection coalescing
-    provideRouter(routes)  // Providing the router configuration
+    provideRouter(routes) ,provideHttpClient() // Providing the router configuration
   ]
 };
