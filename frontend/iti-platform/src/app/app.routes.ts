d2383196f104fa816b17d85104716b149634c225
Essam-sara <saraessam2580@gmail.com>
2025-03-18 15:25:29 +0200

    managing routes

--- frontend/iti-platform/src/app/app.routes.ts
+++ frontend/iti-platform/src/app/app.routes.ts
@@ -4,13 +4,10 @@ import { LoginComponent } from './login/login.component';
 import { MentorshipComponent } from './mentorship/mentorship.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
-const isAdmin = () => localStorage.getItem('user_role') === 'admin';
-
-// Define your routes here
 export const routes: Routes = [
-  { path: 'register', component: RegisterComponent,title: 'Register' }, // /register -> RegisterComponent
-  { path: 'api/login', component: LoginComponent, title: 'Login'},       // /login -> LoginComponent
-  { path: 'api/mentorship', component: MentorshipComponent , title: 'Mentorship' },
+  { path: 'register', component: RegisterComponent, title: 'Register' },
+  { path: 'login', component: LoginComponent, title: 'Login' },
+  { path: 'mentorship', component: MentorshipComponent, title: 'Mentorship' },
   { path: 'admin/dashboard', component: AdminDashboardComponent },
-  { path: '', redirectTo: '/api/login', pathMatch: 'full' }  // Redirect from root ('') to login page
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
