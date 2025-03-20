7cbea45f0a9a7292c3122d0d2ecf33c30f693b5d
Essam-sara <saraessam2580@gmail.com>
2025-03-20 09:49:03 +0200

    Updated app.component.ts with new logic

--- frontend/iti-platform/src/app/app.component.ts
+++ frontend/iti-platform/src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms'; // <-- Import FormsModule here
 import { MentorshipComponent } from './mentorship/mentorship.component'; 
+import { FormsModule } from '@angular/forms';
 
+// import { BrowserModule } from '@angular/platform-browser';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent, FormsModule, MentorshipComponent], // <-- Add the components you need
-  template: '<app-login></app-login>',  // Use the login component in the template
-  styles: [],
-   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, LoginComponent, FormsModule,CommonModule,
+    
+    MatProgressSpinnerModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'iti-hub';
