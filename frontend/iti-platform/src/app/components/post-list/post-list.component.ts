dd76335a8683650f969293c8943957bb2bc5bf1a
zina125 <zina.ragab12@gmail.com>
2025-03-24 00:02:45 +0200

    Add PostList component to display all forum posts with search

---
+++ frontend/iti-platform/src/app/components/post-list/post-list.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { ForumService } from '../../services/forum.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  standalone: true,
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ]
+})
+export class PostListComponent implements OnInit {
+  posts: any[] = [];
+  searchQuery: string = '';
+
+  constructor(
+    private forumService: ForumService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.loadPosts();
+  }
+
+  loadPosts(): void {
+    this.forumService.getPosts().subscribe({
+      next: (response: any) => {
+        this.posts = response;
+
+      },
+      error: (error: any) => {
+        console.error(error);
+      }
+    });
+  }
+  
+
+  goToCreate(): void {
+    this.router.navigate(['/posts/create']);
+  }
+
+  search(): void {
+    this.forumService.searchPosts(this.searchQuery).subscribe({
+      next: (response: any) => {
+        this.posts = response.data;
+      },
+      error: (err: any) => {
+        console.error(err);
+      }
+    });
+  }
+}
