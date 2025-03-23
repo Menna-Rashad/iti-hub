8f4635978c0680757a20cb6bd1f1cec34dd1a6ea
Essam-sara <saraessam2580@gmail.com>
2025-03-23 19:03:04 +0200

    Updated User model to include additional functionality or fix existing issues.

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -24,12 +24,14 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'national_id',
         'role',
         'profile_picture',
         'bio',
         'linkedin',
         'github',
         'is_verified',
+        'email_verified_at',
     ];
 
     /**
@@ -51,6 +53,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'is_verified' => 'boolean',
             'password' => 'hashed',
         ];
     }
