69eb245007d60e10580c910cebd5f1f33875ce08
zina125 <zina.ragab12@gmail.com>
2025-03-23 23:57:42 +0200

    Adjust main app component layout and imports

--- frontend/iti-platform/src/app/app.component.ts
+++ frontend/iti-platform/src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms'; // <-- Import FormsModule here
+import { FormsModule } from '@angular/forms';
 import { MentorshipComponent } from './mentorship/mentorship.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent, FormsModule, MentorshipComponent], // <-- Add the components you need
-  template: '<app-login></app-login>',  // Use the login component in the template
+  imports: [RouterOutlet, FormsModule, 
+
+  ], 
+  template: '<app-login></app-login>',  
   styles: [],
    templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'iti-hub';
