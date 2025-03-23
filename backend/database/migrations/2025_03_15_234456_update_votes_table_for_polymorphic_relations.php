c26cfd7a5151e38f7efadfdb2e8c3bcc0ec1c588
zina125 <zina.ragab12@gmail.com>
2025-03-23 23:55:42 +0200

    Update votes table to support polymorphic relationships

--- backend/database/migrations/2025_03_15_234456_update_votes_table_for_polymorphic_relations.php
+++ backend/database/migrations/2025_03_15_234456_update_votes_table_for_polymorphic_relations.php
@@ -6,25 +6,13 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up()
+    public function up(): void
     {
-        Schema::table('votes', function (Blueprint $table) {
-            $table->dropForeign(['forum_post_id']);
-            $table->dropForeign(['comment_id']);
-            $table->dropColumn(['forum_post_id', 'comment_id']);
-            $table->unsignedBigInteger('votable_id');
-            $table->string('votable_type');
-        });
+
     }
-    
-    public function down() {
-        Schema::table('votes', function (Blueprint $table) {
-            $table->foreignId('forum_post_id')->nullable()->constrained()->onDelete('cascade');
-            $table->foreignId('comment_id')->nullable()->constrained()->onDelete('cascade');
-            $table->dropMorphs('votable');
-        });
+
+    public function down(): void
+    {
+
     }
 };
