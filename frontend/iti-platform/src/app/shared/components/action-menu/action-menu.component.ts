5ab4c91c0a637c93ed02f5aef89abc35d44f785c
Menna-Rashad <mennarashad301@gmail.com>
2025-04-08 20:28:21 +0200

    created action component in shared/components

---
+++ frontend/iti-platform/src/app/shared/components/action-menu/action-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-action-menu',
+  imports: [],
+  templateUrl: './action-menu.component.html',
+  styleUrl: './action-menu.component.css'
+})
+export class ActionMenuComponent {
+
+}
