1450d8f0b02169f47242b61bbf8e4e22b309d550
Essam-sara <saraessam2580@gmail.com>
2025-04-21 11:08:44 +0200

    🧠 Finalize bootstrap for hybrid Angular project setup

--- frontend/iti-platform/src/main.ts
+++ frontend/iti-platform/src/main.ts
@@ -1,29 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { importProvidersFrom } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
-import { AuthInterceptor } from './app/auth.interceptor'; // ✅
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(
-      withInterceptors([AuthInterceptor]) // ✅ بدون new ولا ()=>new
-    ),
-    provideRouter(routes),
-    importProvidersFrom(ReactiveFormsModule),
-    importProvidersFrom(
-      BrowserAnimationsModule,
-      ToastrModule.forRoot({
-        positionClass: 'toast-top-left',
-        timeOut: 3000,
-        closeButton: true,
-        progressBar: true,
-      })
-    ),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
