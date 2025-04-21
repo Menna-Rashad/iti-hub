184198539b5c4c7d4b9a47446b71b36b60379f64
Essam-sara <saraessam2580@gmail.com>
2025-04-21 11:07:12 +0200

    🧭 Update app-routing.module.ts with new routes and guards

--- frontend/iti-platform/src/app/app-routing.module.ts
+++ frontend/iti-platform/src/app/app-routing.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { PostListComponent } from './components/post-list/post-list.component';
-import { CreatePostComponent } from './components/create-post/create-post.component';
-import { PostDetailComponent } from './components/post-detail/post-detail.component';
-// import { OpenProjectListComponent } from './components/open-project-list/open-project-list.component';
-// import { MainContentComponent } from './main-content/main-content.component';
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { PostListComponent } from './components/post-list/post-list.component';
+// import { CreatePostComponent } from './components/create-post/create-post.component';
+// import { PostDetailComponent } from './components/post-detail/post-detail.component';
+// // import { OpenProjectListComponent } from './components/open-project-list/open-project-list.component';
+// // import { MainContentComponent } from './main-content/main-content.component';
 
-const routes: Routes = [
-  // { path: 'open-projects', component: OpenProjectListComponent },
-  { path: 'forum', component: PostListComponent },
-  { path: 'create-post', component: CreatePostComponent },
-  { path: 'post/:id', component: PostDetailComponent },
-  // { path: '', redirectTo: '/forum', pathMatch: 'full' },
-  // { path: 'main-content', component: MainContentComponent },
-];
+// const routes: Routes = [
+//   // { path: 'open-projects', component: OpenProjectListComponent },
+//   { path: 'forum', component: PostListComponent },
+//   { path: 'create-post', component: CreatePostComponent },
+//   { path: 'post/:id', component: PostDetailComponent },
+//   // { path: '', redirectTo: '/forum', pathMatch: 'full' },
+//   // { path: 'main-content', component: MainContentComponent },
+// ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
