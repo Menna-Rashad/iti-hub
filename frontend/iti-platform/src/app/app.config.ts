9a64103e7251dfed4718a85693ff9eb5f6e63c23
Essam-sara <saraessam2580@gmail.com>
2025-04-21 11:06:23 +0200

    🚀 Update core app structure and routing for hybrid module support

--- frontend/iti-platform/src/app/app.config.ts
+++ frontend/iti-platform/src/app/app.config.ts
@@ -1,12 +1,43 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';  // Importing required functions
-import { provideRouter } from '@angular/router';  // Importing the router configuration function
-
-import { routes } from './app.routes';  // Importing routes from the app.routes.ts file
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { routes } from './app.routes';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { importProvidersFrom } from '@angular/core';
+import { ToastrModule } from 'ngx-toastr';
+import { AuthConfig, OAuthModule } from 'angular-oauth2-oidc';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),  // Enabling change detection coalescing
-    provideRouter(routes) ,provideHttpClient() // Providing the router configuration
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+    importProvidersFrom(
+      OAuthModule.forRoot(),
+      ToastrModule.forRoot({
+        positionClass: 'toast-top-left',
+        timeOut: 3000,
+        closeButton: true,
+        progressBar: true,
+      })
+    ),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+    {
+      provide: AuthConfig,
+      useValue: {
+        issuer: 'https://accounts.google.com',
+        redirectUri: window.location.origin + '/login',
+        clientId: '136248172784-g14vvg68t7sh2srb2oi9snebpkkhegcp.apps.googleusercontent.com',
+        responseType: 'code',
+        scope: 'openid profile email',
+        showDebugInformation: true,
+      } as AuthConfig,
+    },
+  ],
 };
