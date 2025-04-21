7e0a1f2391fbc473cc39935612dd28379e46d934
Essam-sara <saraessam2580@gmail.com>
2025-04-21 11:07:55 +0200

    🤝 Improve mentorship component logic and style

--- frontend/iti-platform/src/app/mentorship/mentorship.component.ts
+++ frontend/iti-platform/src/app/mentorship/mentorship.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mentorship',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './mentorship.component.html',
   styleUrls: ['./mentorship.component.css'],
