2c6e86e671ac3952470961716a8e2b40b83bf2d1
Menna-Rashad <mennarashad301@gmail.com>
2025-04-23 22:28:27 +0200

    improved app component for footer and navbar usage

--- frontend/iti-platform/src/app/app.component.ts
+++ frontend/iti-platform/src/app/app.component.ts
@@ -3,19 +3,16 @@ import { RouterOutlet } from '@angular/router';
 // import { LoginComponent } from './login/login.component';
 // import { FormsModule } from '@angular/forms'; // <-- Import FormsModule here
 // import { MentorshipComponent } from './mentorship/mentorship.component'; // <-- Import the MentorshipComponent here
-import { NavbarComponent } from './navbar/navbar.component'; // <-- Import the NavbarComponent here
-
 // import { BrowserModule } from '@angular/platform-browser';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout/layout.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
-  template: `
-    <app-navbar></app-navbar>
-    <router-outlet></router-outlet>
-  `,
+  imports: [RouterOutlet, LayoutComponent],
+  templateUrl: './app.component.html' ,
   styleUrls: ['./app.component.css']
 })
 
