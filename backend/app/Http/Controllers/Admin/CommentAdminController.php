57c77cb818312c54fdddc3a22350bcea63771b48
zina125 <zina.ragab12@gmail.com>
2025-04-27 10:08:17 +0300

    Update CommentAdminController: Add admin log after comment deletion

--- backend/app/Http/Controllers/Admin/CommentAdminController.php
+++ backend/app/Http/Controllers/Admin/CommentAdminController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Comment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\AdminLog;
 
 class CommentAdminController extends Controller
 {
@@ -28,6 +29,11 @@ class CommentAdminController extends Controller
         $comment = Comment::findOrFail($id);
         $comment->delete();
 
+        AdminLog::create([
+            'admin_id' => auth()->id(),
+            'action' => "Deleted comment ID: {$id}",
+        ]);
+    
         return response()->json(['message' => 'Comment deleted successfully']);
     }
 }
