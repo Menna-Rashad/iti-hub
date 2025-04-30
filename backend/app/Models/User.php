fdf1fae1e1cdf13884bfc25fc76dea2ea64316b4
zina125 <zina.ragab12@gmail.com>
2025-04-30 19:46:14 +0300

    fix: rename posts relation to forumPosts for consistency

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -92,4 +92,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(TicketNotification::class);
     }
+    public function supportTickets()
+{
+    return $this->hasMany(\App\Models\SupportTicket::class);
+}
+
 }
