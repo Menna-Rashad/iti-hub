f8dae1036ee4ebdc3ea6b793db3edd383baa7174
Essam-sara <saraessam2580@gmail.com>
2025-05-01 12:34:35 +0300

    Created migration for tracks table

---
+++ backend/database/migrations/2025_05_01_062248_create_tracks_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTracksTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->foreignId('program_id')->constrained()->onDelete('cascade');
+            $table->foreignId('department_id')->constrained()->onDelete('cascade');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tracks');
+    }
+}
