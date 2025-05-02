ee8558e2efe8ce00878ced60546e031ed34050a6
Essam-sara <saraessam2580@gmail.com>
2025-05-03 01:15:07 +0300

    Modified backend/routes/channels.php

--- backend/routes/channels.php
+++ backend/routes/channels.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
-// قنوات البث الخاصة بالمستخدمين
-Broadcast::channel('user.{userId}', function ($user, $userId) {
-    return (int) $user->id === (int) $userId;
-});
 
-// قناة أخبار admin التي يتابعها جميع المستخدمين
-Broadcast::channel('admin-news', function ($user) {
-    return $user->role === 'admin'; // تأكد أن المستخدم من نوع admin
+Broadcast::channel('notifications', function ($user) {
+    return $user != null;
 });
