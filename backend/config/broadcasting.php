f112105ff96d3531b4399cbe2c09cf16a2c31dca
Essam-sara <saraessam2580@gmail.com>
2025-05-03 01:15:07 +0300

    Modified backend/config/broadcasting.php

--- backend/config/broadcasting.php
+++ backend/config/broadcasting.php
@@ -1,15 +1,25 @@
 <?php
 
 return [
-    'default' => env('BROADCAST_DRIVER', 'redis'),
+    'default' => env('BROADCAST_DRIVER', 'database'),
 
-    'connections' => [
-        'redis' => [
-            'driver' => 'redis',
-            'connection' => 'default',
-        ],
-        'log' => [
-            'driver' => 'log',
+'connections' => [
+    'database' => [
+        'driver' => 'database',
+        'connection' => null, // لا حاجة للاتصال بقاعدة بيانات أخرى هنا
+    ],
+    'pusher' => [
+        'driver' => 'pusher',
+        'key' => env('PUSHER_APP_KEY'),
+        'secret' => env('PUSHER_APP_SECRET'),
+        'app_id' => env('PUSHER_APP_ID'),
+        'options' => [
+            'cluster' => env('PUSHER_APP_CLUSTER'),
+            'useTLS' => true,
         ],
     ],
+    // ... إضافات أخرى مثل redis
+],
+
+
 ];
